b2049d8b0f5e2dbf57b802b90749ce32f9e0bf57
appmdev <appdeve@mail.ru>
2022-01-18 11:52:58 +0000

    Initial Commit

---
+++ SportNewsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SportNewsApp
+//
+//  Created by Mac on 18/01/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
